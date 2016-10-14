b975639880a574aaf591766fc7696eb43459fe46
adi-botak- <adityautamawijaya@gmail.com>
2016-10-14 14:16:49 +0800

    Only extracts the IMDB ID of a movie

--- info_api.rb
+++ info_api.rb
@@ -5,32 +5,31 @@ credentials = YAML.load(File.read('credentials.yml'))
 response = {}
 results = {}
 
-# Airbnb search reviews API
-listing_id = '2056659'
-role = 'all'
-review_response =
-  HTTP.get('https://api.airbnb.com/v2/reviews', 
-            params: { client_id: credentials[:client_id],
-                      listing_id: listing_id,
-                      role: role })
-response[:airbnb_review] = review_response
-reviews = JSON.load(review_response.to_s)['reviews']
-results[:reviews] = reviews
-
 # Find desired movie
-keyword = 'star wars'
 movie_response = HTTP.get(
   "http://www.omdbapi.com/",
   params: {
-    t: keyword,
+    t: credentials[:keyword],
     y: '',
     plot: 'short',
     r: 'json'
   })
 
 response[:movie] = movie_response
-movie = JSON.load(movie_response.to_s)
+movie = JSON.load(movie_response.to_s)['imdbID']
 results[:movie] = movie
+
+# Get filming locations from IMDB website
+
+# Airbnb search reviews API
+airbnb_response =
+  HTTP.get('https://api.airbnb.com/v2/reviews', 
+            params: { client_id: credentials[:airbnb_client_id],
+                      listing_id: credentials[:airbnb_listing_id],
+                      role: 'all' })
+response[:airbnb] = airbnb_response
+reviews = JSON.load(airbnb_response.to_s)['reviews']
+results[:reviews] = reviews
 
 File.write('response.yml', response.to_yaml)
 File.write('results.yml', results.to_yaml)
