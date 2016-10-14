1f1cff6011c825fccc8af187da3e459f516d8477
A-n-d-y-W-e-n <a409052003@gmail.com>
2016-10-14 19:31:30 +0800

    merge tests

--- spec/movlog_spec.rb
+++ spec/movlog_spec.rb
@@ -4,6 +4,7 @@ require 'yaml'
 
 require './lib/movie.rb'
 require './lib/skyscanner.rb'
+require './lib/airbnb.rb'
 
 CREDENTIALS = YAML.load(File.read('config/credentials.yml'))
 
@@ -16,15 +17,22 @@ describe 'Movie specifications' do
 
   it 'should get routes suggested by Skyscanner' do
     routes = Skyscanner::Routes.new(
-    	apiKey: CREDENTIALS[:skyscanner_api_key], 
-    	market: 'GB', 
-    	currency: 'GBP', 
-    	locale: 'en-GB', 
-    	originPlace: 'LON', 
-    	destinationPlace: 'UK', 
-    	outboundPartialDate: 'anytime', 
+    	apiKey: CREDENTIALS[:skyscanner_api_key],
+    	market: 'GB',
+    	currency: 'GBP',
+    	locale: 'en-GB',
+    	originPlace: 'LON',
+    	destinationPlace: 'UK',
+    	outboundPartialDate: 'anytime',
     	inboundPartialDate: 'anytime'
     )
     routes.routes.length.must_be :>, 0
+  end
+
+  it 'should get the room info of a location' do
+    roomsinfo = Airbnb::Roomsinfo.new(airbnb_client_id: CREDENTIALS[:airbnb_client_id], location: "Hsinchu")
+
+    rooms = roomsinfo.rooms
+    rooms.length.must_be :>, 0
   end
 end
