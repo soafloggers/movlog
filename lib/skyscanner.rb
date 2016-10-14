fabc98d993b03e42d012b8ccfcea128d4214b40d
Yu-Shiang <z58085111@gmail.com>
2016-10-14 19:12:45 +0800

    1. fix Skyscanner module error 2. add skyscanner test case

--- lib/skyscanner.rb
+++ lib/skyscanner.rb
@@ -2,25 +2,28 @@ require 'http'
 
 module Skyscanner
   class Routes
+    attr_reader :apiKey, :request_url
     attr_reader :origin_place, :destination_place, :outbound_partial_date, :inbound_partial_date
     attr_reader :routes, :quotes, :places
 
-    def initialize(apiKey, market, currency, locale, originPlace, destinationPlace, outboundPartialDate='anytime', inboundPartialDate='anytime')
+    def initialize(apiKey:, market:, currency:, locale:, originPlace:, destinationPlace:, outboundPartialDate:, inboundPartialDate:)
+      @apiKey = apiKey
       @origin_place = origin_place
       @destination_place = destination_place
       @outbound_partial_date = outbound_partial_date
       @inbound_partial_date = inbound_partial_date
-
+      @request_url = 'http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/'+market+'/'+currency+'/'+locale+'/'+originPlace+'/'+destinationPlace+'/'+outboundPartialDate+'/'+inboundPartialDate
+      
     end
 
     def routes
       return @routes if @routes
 
       skyscanner_routes_response = HTTP.get(
-        'http://partners.api.skyscanner.net/apiservices/browseroutes/v1.0/'+market+'/'+currency+'/'+locale+'/'+originPlace+'/'+destinationPlace+'/'+outboundPartialDate+'/'+inboundPartialDate,
+        @request_url,
         params: {
-          apiKey: credentials[:skyscanner_api_key]
-        })
+          apiKey: @apiKey
+      })
       routes = JSON.load(skyscanner_routes_response.to_s)
       @routes = routes['Routes']
       @quotes = routes['Quotes']
