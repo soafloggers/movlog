781080545c6bdb92e27e9b6db303912b2f7df4ab
A-n-d-y-W-e-n <a409052003@gmail.com>
2016-10-14 16:23:56 +0800

    add airbnb rooms API

--- info_api.rb
+++ info_api.rb
@@ -23,13 +23,23 @@ results[:movie] = movie
 
 # Airbnb search reviews API
 airbnb_response =
-  HTTP.get('https://api.airbnb.com/v2/reviews', 
+  HTTP.get('https://api.airbnb.com/v2/reviews',
             params: { client_id: credentials[:airbnb_client_id],
                       listing_id: credentials[:airbnb_listing_id],
                       role: 'all' })
 response[:airbnb] = airbnb_response
 reviews = JSON.load(airbnb_response.to_s)['reviews']
 results[:reviews] = reviews
+
+# Airbnb search rooms API
+airbnb_rooms_response =
+  HTTP.get('https://api.airbnb.com/v2/search_results',
+            params: { client_id: '3092nxybyb0otqw18e8nh5nty',
+                      #locale: credentials[:airbnb_locale],
+                    })
+response[:airbnb_rooms] = airbnb_rooms_response
+rooms = JSON.load(airbnb_rooms_response.to_s)['rooms']
+results[:rooms] = rooms
 
 File.write('response.yml', response.to_yaml)
 File.write('results.yml', results.to_yaml)
