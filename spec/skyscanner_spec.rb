b5892b66078e735c7d30de0ce6b511cb47515c85
Yu-Shiang <z58085111@gmail.com>
2016-10-21 17:59:58 +0800

    skyscanner_spec add VCR & webmock

--- spec/skyscanner_spec.rb
+++ spec/skyscanner_spec.rb
@@ -2,10 +2,21 @@
 require_relative 'spec_helper.rb'
 
 describe 'Skyscanner specifications' do
+  VCR.configure do |c|
+    c.cassette_library_dir = CASSETTES_FOLDER
+    c.hook_into :webmock
+    c.filter_sensitive_data('<SKYSCANNER_API_KEY>') { CREDENTIALS[:skyscanner_api_key] }
+  end
+
   before do
+    VCR.insert_cassette CASSETTE_FILE_2, record: :new_episodes
     @skyscanner_api = Skyscanner::SkyscannerApi.new(
       api_key: CREDENTIALS[:skyscanner_api_key]
     )
+  end
+
+  after do
+    VCR.eject_cassette
   end
 
   it 'should get routes suggested by Skyscanner' do
