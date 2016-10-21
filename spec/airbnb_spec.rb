6993a51d6595aba74b009f7eb924a94ee96dd147
A-n-d-y-W-e-n <a409052003@gmail.com>
2016-10-21 17:53:05 +0800

    add VCR to airbnb

--- spec/airbnb_spec.rb
+++ spec/airbnb_spec.rb
@@ -2,10 +2,22 @@
 require_relative 'spec_helper.rb'
 
 describe 'Airbnb specifications' do
+  VCR.configure do |c|
+    c.cassette_library_dir = CASSETTES_FOLDER
+    c.hook_into :webmock
+    c.filter_sensitive_data('<airbnb_client_id>') { CREDENTIALS[:airbnb_client_id] }
+  end
+
   before do
+    VCR.insert_cassette CASSETTE_FILE_3, record: :new_episodes
     @airbnb_api = Airbnb::AirbnbApi.new(
       client_id: CREDENTIALS[:airbnb_client_id]
     )
+
+  end
+
+  after do
+    VCR.eject_cassette
   end
 
   it 'should get the room info of a location' do
