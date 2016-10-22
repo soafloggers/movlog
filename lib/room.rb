fc8e9d0491e1cea8c5fc49672fa39ff1fbfebce2
A-n-d-y-W-e-n <a409052003@gmail.com>
2016-10-22 14:57:04 +0800

    add some info of room

--- lib/room.rb
+++ lib/room.rb
@@ -24,12 +24,17 @@ module Airbnb
     private
 
     def room(item)
-      item = item['listing']
+      item = item
       {
-        city: item['city'],
-        name: item['name'],
-        pic_url: item['picture_url'],
-        id: item['id']
+        city: item['listing']['city'],
+        name: item['listing']['name'],
+        pic_url: item['listing']['picture_url'],
+        id: item['listing']['id'],
+        person_capacity: item['listing']['person_capacity'],
+        primary_host: item['listing']['primary_host'],
+        star_rating: item['listing']['star_rating'],
+        listing_currency: item['pricing_quote']['listing_currency'],
+        nightly_price: item['pricing_quote']['nightly_price']
       }
     end
   end
